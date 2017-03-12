fd643f21483e6b998076209b389ce204a1dcb24a
Paula Calhelha <pcalhelha@gmail.com>
2017-03-12 11:09:22 -0400

    Add variables

--- shipping.rb
+++ shipping.rb
@@ -1,1 +1,26 @@
-#### calculate shipping fees
+Flat_fee = 0.5
+Person_fee = .0012
+Pharma_fee = .0075
+Food_fee = .13
+Electro_fee = .02
+No_fee = 0
+
+Pharma_type = 'pharma'
+Food_type = 'food'
+Electro_type = 'electro'
+
+material_type_fee = 0.00
+final_price = 0.00
+number_persons_fee = 0
+other_markups = 0.00
+
+
+
+def calc_shipping_fee(original_price, number_persons, material_type)
+
+end
+
+
+
+
+
