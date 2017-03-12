7a4159c90ed49cc910e8fc432a985dc9820b96ef
Paula Calhelha <pcalhelha@gmail.com>
2017-03-12 11:41:28 -0400

    Fixed constant values Added a default case for the material fee Fixed final_price logic

--- shipping.rb
+++ shipping.rb
@@ -1,11 +1,11 @@
-Flat_fee = 0.5
-Person_fee = 0.0012
-Pharma_fee = 0.0075
+Flat_fee = 0.05
+Person_fee = 0.012
+Pharma_fee = 0.075
 Food_fee = 0.13
 Electro_fee = 0.02
 No_fee = 0
 
-Pharma_type = 'pharma'
+Pharma_type = 'drugs'
 Food_type = 'food'
 Electro_type = 'electro'
 
@@ -24,14 +24,16 @@ def calc_shipping_fee(original_price, number_persons, material_type)
     material_type_fee = Food_fee
   when Electro_type
     material_type_fee = Electro_fee
+  else
+    material_type_fee = No_fee
   end
 
   number_persons_fee = number_persons * Person_fee
 
-  other_markups = material_type + number_persons_fee
+  other_markups = material_type_fee + number_persons_fee
 
   price = (original_price + (original_price * Flat_fee))
-  final_price = price + (original_price * other_markups)
+  final_price = (price + (price * other_markups))
 
 end
 
--- shipping_test.rb
+++ shipping_test.rb
@@ -1,3 +1,5 @@
 require "./shipping"
 
 puts calc_shipping_fee(1299.99, 3, 'food')
+puts calc_shipping_fee(5432.0, 1, 'drugs')
+puts calc_shipping_fee(12456.95, 4, 'books')
